db1693f09ce27e1a16b7633726529234a52cfcc0
vitodursopettersson <vitodurso98@gmail.com>
2023-11-28 12:03:44 +0100

    feat: hello vuejs

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Hello Vue!',
+            imageVueJs: 'vuejs.png'
+
+        }
+    }
+}).mount('#app')
